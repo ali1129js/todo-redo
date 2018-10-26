357b7d4918aedbb161a91b25634a55396449e627
ali1129js <ali1129js@gmail.com>
2018-10-26 11:59:54 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
+/**
+ * @Author: Ali
+ * @Date:   2018-10-26T11:54:42+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-10-26T11:58:22+02:00
+ */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  render(){
+      return (
+        <div>
+        <form>
+        <input placeholder="Task here" />
+        <button> Add Task</button>
+        </form>
+        </div>
+      )
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
+/**
+ * @Author: Ali
+ * @Date:   2018-10-26T11:52:48+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-10-26T11:52:53+02:00
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
